446285fbb999d06da2004a1b836974144152992c
LutfiAnx <giannaela1@gmail.com>
2025-03-29 19:52:34 +0700

    Update processController.js

--- controller/processController.js
+++ controller/processController.js
@@ -10,10 +10,10 @@ async function processController(url) {
             rejectUnauthorized: false  
         }),
         headers: {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Referer": "https://dood.li/",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Referer": "https://dood.li/"
         }
     });
 

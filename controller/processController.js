02dedf07b3e9a3c8faaf07110979d9c125a50e35
F4Ray <f4ray@F4Rays-MacBook.local>
2025-03-30 00:47:14 +0700

    f

--- controller/processController.js
+++ controller/processController.js
@@ -9,147 +9,104 @@ const chromium = require('@sparticuz/chromium')
 const pluginStealth = require('puppeteer-extra-plugin-stealth')
 const { executablePath } = require('puppeteer')
 
+
 async function processController(url) {
-    if (url.startsWith("<!doctype html>")) {
-        const match = htmlFromCloudflare.match(/\$.get\('([^']+)',\s*function\(data\)/)
-        console.log("ini match bukan url "+match)
+    console.log("Starting the process to fetch the Doodstream CDN link...");
+    
+    const session = axios.create({
+        httpsAgent: new https.Agent({  
+            rejectUnauthorized: false  
+        }),
+        // headers: {
+        //     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        //     "Accept-Encoding": "gzip, deflate, br, zstd",
+        //     "Referer": "https://dood.li/",
+        //     "User-Agent": "Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"
+        // }
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Referer": "https://do7go.com/"
+          }
+    });
 
-        if (match) {
-            const urlInsideGet = match[1];
-            console.log(`URL found inside the response: ${urlInsideGet}`);
-            
-            const lastValueMatch = urlInsideGet.match(/\/([^/]+)$/);
-            if (!lastValueMatch) {
-                console.error("Error: Unable to extract token from URL.");
-                return null;
-            }
-            const token = lastValueMatch[1];
-            const secondUrl = `https://dood.li${urlInsideGet}`;
-            console.log(`Constructed URL for second request: ${secondUrl}`);
-            
-            const secondResponse = await session.get(secondUrl);
-            if (secondResponse.status === 200) {
-                console.log("Successfully fetched the secondary URL contents.");
-                
-                const part1 = secondResponse.data;
-                console.log("ini part 1");
-                console.log(part1);
-                
-                const randomString = generateRandomString();
-                const expiry = Date.now();
-                const part2 = `${randomString}?token=${token}&expiry=${expiry}`;
-                const finalUrl = `${part1}${part2}`;
-                
-                console.log(`Generated random string: ${randomString}`);
-                console.log(`Token: ${token}`);
-                console.log(`Expiry timestamp: ${expiry}`);
-                console.log(`Doodstream CDN link generated: ${finalUrl}`);
-                return finalUrl;
-            } else {
-                console.error("Error: Unable to fetch the contents of the secondary URL.");
-            }
-        } else {
-            console.error("Error: Unable to find the required URL inside the initial response.");
-        }
-    }
-    else {
-        console.log("Starting the process to fetch the Doodstream CDN link...");
-        
-        const session = axios.create({
-            httpsAgent: new https.Agent({  
-                rejectUnauthorized: false  
-            }),
-            // headers: {
-            //     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            //     "Accept-Encoding": "gzip, deflate, br, zstd",
-            //     "Referer": "https://dood.li/",
-            //     "User-Agent": "Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"
-            // }
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Referer": "https://dood.li/"
-            }
-        });
-
-        const fileCode = extractFileCode(url);
-        console.log(fileCode)
-        if (!fileCode) {
-            console.error("Error: Can't find the file code in the URL.");
-            console.error(`Are you sure ${url} is correct?`);
-            return null;
-        }
-
-        const doodstreamUrl = `https://dood.li/e/${fileCode}`;
-        console.log(`File code extracted: ${fileCode}`);
-        console.log(`Constructed Doodstream URL: ${doodstreamUrl}`);
-
-        try {
-            console.log("ini "+ doodstreamUrl);
-            // const response = await session.get({url: doodstreamUrl, "rejectUnauthorized": false});
-            const response = await session.get(doodstreamUrl);
-            if (response.status === 200) {
-                console.log("Successfully fetched the Doodstream page.");
-                console.log(response.data);
-                const match = response.data.match(/\$.get\('([^']+)',\s*function\(data\)/);
-                
-
-                if (response.data.includes('captcha-player')) {
-                    console.log('Captcha detected! Taking action...');
-                    const htmlFromCloudflare = await getHtmlThoughCloudflare(doodstreamUrl);
-
-                    const match = htmlFromCloudflare.match(/\$.get\('([^']+)',\s*function\(data\)/)
-                    console.log("ini match "+match)
-                } else {
-                    console.log('No CAPTCHA, processing data...');
-                    if (match) {
-                        const urlInsideGet = match[1];
-                        console.log(`URL found inside the response: ${urlInsideGet}`);
-                        
-                        const lastValueMatch = urlInsideGet.match(/\/([^/]+)$/);
-                        if (!lastValueMatch) {
-                            console.error("Error: Unable to extract token from URL.");
-                            return null;
-                        }
-                        const token = lastValueMatch[1];
-                        const secondUrl = `https://dood.li${urlInsideGet}`;
-                        console.log(`Constructed URL for second request: ${secondUrl}`);
-                        
-                        const secondResponse = await session.get(secondUrl);
-                        if (secondResponse.status === 200) {
-                            console.log("Successfully fetched the secondary URL contents.");
-                            
-                            const part1 = secondResponse.data;
-                            console.log("ini part 1");
-                            console.log(part1);
-                            
-                            const randomString = generateRandomString();
-                            const expiry = Date.now();
-                            const part2 = `${randomString}?token=${token}&expiry=${expiry}`;
-                            const finalUrl = `${part1}${part2}`;
-                            
-                            console.log(`Generated random string: ${randomString}`);
-                            console.log(`Token: ${token}`);
-                            console.log(`Expiry timestamp: ${expiry}`);
-                            console.log(`Doodstream CDN link generated: ${finalUrl}`);
-                            return finalUrl;
-                        } else {
-                            console.error("Error: Unable to fetch the contents of the secondary URL.");
-                        }
-                    } else {
-                        console.error("Error: Unable to find the required URL inside the initial response.");
-                    }
-                }
-                
-            } else {
-                console.error("Error: Unable to fetch the Doodstream page. HTTP status code:", response.status);
-            }
-        } catch (error) {
-            console.error("Error during request:", error.message);
-        }
+    const fileCode = extractFileCode(url);
+    console.log(fileCode)
+    if (!fileCode) {
+        console.error("Error: Can't find the file code in the URL.");
+        console.error(`Are you sure ${url} is correct?`);
         return null;
     }
+
+    const doodstreamUrl = `https://do7go.com/e/${fileCode}`;
+    console.log(`File code extracted: ${fileCode}`);
+    console.log(`Constructed Doodstream URL: ${doodstreamUrl}`);
+
+    try {
+        console.log("ini "+ doodstreamUrl);
+        // const response = await session.get({url: doodstreamUrl, "rejectUnauthorized": false});
+        const response = await session.get(doodstreamUrl);
+        if (response.status === 200) {
+            console.log("Successfully fetched the Doodstream page.");
+            console.log(response.data);
+            const match = response.data.match(/\$.get\('([^']+)',\s*function\(data\)/);
+            
+
+            if (response.data.includes('captcha-player')) {
+                console.log('Captcha detected! Taking action...');
+                const htmlFromCloudflare = await getHtmlThoughCloudflare(doodstreamUrl);
+
+                const match = htmlFromCloudflare.match(/\$.get\('([^']+)',\s*function\(data\)/)
+                console.log("ini match "+match)
+              } else {
+                console.log('No CAPTCHA, processing data...');
+                if (match) {
+                    const urlInsideGet = match[1];
+                    console.log(`URL found inside the response: ${urlInsideGet}`);
+                    
+                    const lastValueMatch = urlInsideGet.match(/\/([^/]+)$/);
+                    if (!lastValueMatch) {
+                        console.error("Error: Unable to extract token from URL.");
+                        return null;
+                    }
+                    const token = lastValueMatch[1];
+                    const secondUrl = `https://do7go.com${urlInsideGet}`;
+                    console.log(`Constructed URL for second request: ${secondUrl}`);
+                    
+                    const secondResponse = await session.get(secondUrl);
+                    if (secondResponse.status === 200) {
+                        console.log("Successfully fetched the secondary URL contents.");
+                        
+                        const part1 = secondResponse.data;
+                        console.log("ini part 1");
+                        console.log(part1);
+                        
+                        const randomString = generateRandomString();
+                        const expiry = Date.now();
+                        const part2 = `${randomString}?token=${token}&expiry=${expiry}`;
+                        const finalUrl = `${part1}${part2}`;
+                        
+                        console.log(`Generated random string: ${randomString}`);
+                        console.log(`Token: ${token}`);
+                        console.log(`Expiry timestamp: ${expiry}`);
+                        console.log(`Doodstream CDN link generated: ${finalUrl}`);
+                        return finalUrl;
+                    } else {
+                        console.error("Error: Unable to fetch the contents of the secondary URL.");
+                    }
+                } else {
+                    console.error("Error: Unable to find the required URL inside the initial response.");
+                }
+            }
+            
+        } else {
+            console.error("Error: Unable to fetch the Doodstream page. HTTP status code:", response.status);
+        }
+    } catch (error) {
+        console.error("Error during request:", error.message);
+    }
+    return null;
 }
 
 function extractFileCode(url) {
@@ -164,7 +121,7 @@ function generateRandomString(length = 10) {
 
 
 const getHtmlThoughCloudflare = async (url) => {
-    //puppeteer.use(pluginStealth())
+    // puppeteer.use(pluginStealth())
 
     try {
         browser = await puppeteerCore.launch({

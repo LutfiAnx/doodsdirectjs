bed23a4e42551328c89d01cfa05d54e24e4090b1
LutfiAnx <giannaela1@gmail.com>
2025-03-29 19:03:18 +0700

    initial

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+// const processDoodstreamUrl = require('./controller/fetchController');
+// const processController = require('./controller/processController');
+const { processController } = require('./controller/processController');
+
+const bodyParser = require('body-parser')
+
+app.use(express.json())
+
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/main', (req,res) => {
+    res.render('main.html')
+})
+
+app.post('/fetch-link', async (req, res) => {
+    const url = req.body.url;
+
+    try {
+        const result = await processController(url);
+        res.json({ result });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
